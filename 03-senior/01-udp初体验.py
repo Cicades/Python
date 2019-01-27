0f37fc904dacccff1202c658d541cc027b19416a
cicades <cicades@qq.com>
2019-01-27 21:11:13 +0800

    start senior learning

---
+++ 03-senior/01-udp初体验.py
@@ -0,0 +1,26 @@
+from socket import *
+
+def main():
+		
+	# 1.创建套接字
+	udp_socket = socket(AF_INET, SOCK_DGRAM)
+	while True:
+	
+		# 2.准备发送内容
+		send_data = input('请输入要发送的内容(exit退出):')
+		if send_data == 'exit':
+			break
+		# 3.准备接受方地址
+		dest_addr = ('192.168.0.102', 8080)
+
+		# 4.发送数据
+		# b可以将字符转换为二进制数据
+		# udp_socket.sendto(b'hello world', dest_addr)
+		udp_socket.sendto(send_data.encode('utf-8'), dest_addr)
+	# 5.关闭套接字
+	udp_socket.close()
+
+if __name__ == '__main__':
+	main()
+
+
