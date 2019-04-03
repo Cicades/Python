637f2e151650a6034fc7ac08c889b3db1902ea62
hyf <hyf1499812174@gmail.com>
2019-04-04 00:37:04 +0800

    dailyfresh:implement the function of adding to the cart.

--- 03-Django/03-天天生鲜/dailyfresh/apps/cart/urls.py
+++ 03-Django/03-天天生鲜/dailyfresh/apps/cart/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import url
+from cart.views import  CarAddView
 
 urlpatterns = [
-
+    url(r'^add$', CarAddView.as_view(), name='add'),  # 添加商品
 ]
--- 03-Django/03-天天生鲜/dailyfresh/apps/cart/views.py
+++ 03-Django/03-天天生鲜/dailyfresh/apps/cart/views.py
@@ -1,3 +1,42 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.generic import View
+from goods.models import GoodsSKU
+from django_redis import get_redis_connection
+
 
 # Create your views here.
+class CarAddView(View):
+    """添加商品到购物车"""
+    def post(self, request):
+        """添加商品"""
+        sku_id = request.POST.get('sku_id')
+        sku_num = request.POST.get('sku_num')
+        user = request.user
+        if not user.is_authenticated():
+            return JsonResponse({'res': -1, 'errmsg': '用户尚未登录！'})
+        if not all([sku_num, sku_id]):
+            '''数据不完整'''
+            return JsonResponse({'res': 0, 'errmsg': '数据不完整！'})
+        try:
+            sku_num = int(sku_num)
+        except ValueError:
+            return JsonResponse({'res': 1, 'errmsg': '数据格式有误！'})
+        try:
+            sku = GoodsSKU.objects.get(id=sku_id)
+        except GoodsSKU.DoesNotExist:
+            return JsonResponse({'res': 1, 'errmsg': '数据格式有误！'})
+        if sku.stock < sku_num:
+            return JsonResponse({'res': 2, 'errmsg': '库存不足！'})
+        # 连接redis
+        redis_client = get_redis_connection('default')
+        h_name = 'cart_{0}'.format(user.id)
+        old_count = redis_client.hget(h_name, sku_id)  # 去购物车对应sku的数量
+        old_count = 0 if old_count is None else int(old_count)
+        old_count += sku_num
+        redis_client.hset(h_name, sku_id, old_count)
+        context = {
+            'res': 'OK',
+            'cartCount': redis_client.hlen(h_name)
+        }
+        return JsonResponse(context)
